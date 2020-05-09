14b78d69b2505f885669e4c5c6dde466e465a659
Inna Widener <iwide2019@gmail.com>
2020-05-09 09:56:24 -0500

    Updates

---
+++ precache-manifest.78cd86cbb27b792e36fd6c35edcee93b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a502e33e6d9a94bf3c003af87be81b",
+    "url": "/Employee-Directory-React/index.html"
+  },
+  {
+    "revision": "4cb603d6bd2e9d1ed2d0",
+    "url": "/Employee-Directory-React/static/css/2.70380555.chunk.css"
+  },
+  {
+    "revision": "9b27b322e66da175c050",
+    "url": "/Employee-Directory-React/static/css/main.7c36a801.chunk.css"
+  },
+  {
+    "revision": "4cb603d6bd2e9d1ed2d0",
+    "url": "/Employee-Directory-React/static/js/2.df698dbe.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Employee-Directory-React/static/js/2.df698dbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b27b322e66da175c050",
+    "url": "/Employee-Directory-React/static/js/main.3d02342c.chunk.js"
+  },
+  {
+    "revision": "14f41ab4902ef9ff27ad",
+    "url": "/Employee-Directory-React/static/js/runtime-main.e845c0c5.js"
+  },
+  {
+    "revision": "b7e0fe211f5ed1f09151051844beb776",
+    "url": "/Employee-Directory-React/static/media/backgroundrose.b7e0fe21.jpg"
+  }
+]);
